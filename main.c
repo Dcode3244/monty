30ae285ddc814c528aee6aaca1860de1c867c9e2
Dcode3244 <dagimourgie111@gmail.com>
2022-10-06 23:34:28 +0300

    entrance point

---
+++ main.c
@@ -0,0 +1,171 @@
+#include "monty.h"
+
+char **opcode_toks = NULL;
+
+/**
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
+{
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
+
+	if (argc != 2)
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (open_error(argv[1]));
+	exit_code = monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
+}
+
+
+/**
+ * monty - Primary function to execute a Monty bytecodes script.
+ * @script_fd: File descriptor for an open Monty bytecodes script.
+ * Return: EXIT_SUCCESS on success, respective error code on failure.
+ */
+int monty(FILE *script_fd)
+{
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t len = 0, exit_status = EXIT_SUCCESS;
+	unsigned int line_number = 0, prev_tok_len = 0;
+	void (*op_func)(stack_t**, unsigned int);
+
+	if (init_stack(&stack) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+
+	while (getline(&line, &len, script_fd) != -1)
+	{
+		line_number++;
+		opcode_toks = _strtok(line, DELIMS);
+		if (opcode_toks == NULL)
+		{
+			if (is_empty_line(line, DELIMS))
+				continue;
+			free_stack(&stack);
+			return (malloc_error());
+		}
+		else if (opcode_toks[0][0] == '#') /* comment line */
+		{
+			free_tokens();
+			continue;
+		}
+		op_func = get_op_func(opcode_toks[0]);
+		if (op_func == NULL)
+		{
+			free_stack(&stack);
+			exit_status = unknown_op_error(opcode_toks[0], line_number);
+			free_tokens();
+			break;
+		}
+		prev_tok_len = token_arr_len();
+		op_func(&stack, line_number);
+		if (token_arr_len() != prev_tok_len)
+		{
+			if (opcode_toks && opcode_toks[prev_tok_len])
+				exit_status = atoi(opcode_toks[prev_tok_len]);
+			else
+				exit_status = EXIT_FAILURE;
+			free_tokens();
+			break;
+		}
+		free_tokens();
+	}
+	free_stack(&stack);
+
+	if (line && *line == 0)
+	{
+		free(line);
+		return (malloc_error());
+	}
+
+	free(line);
+	return (exit_status);
+}
+
+/**
+ * token_arr_len - Gets the length of current opcode_toks.
+ *
+ * Return: Length of current opcode_toks (as int).
+ */
+unsigned int token_arr_len(void)
+{
+	unsigned int toks_len = 0;
+
+	while (opcode_toks[toks_len])
+		toks_len++;
+	return (toks_len);
+}
+
+/**
+ * is_empty_line - Checks if a line read from getline only contains delimiters.
+ * @line: A pointer to the line.
+ * @delims: A string of delimiter characters.
+ *
+ * Return: If the line only contains delimiters - 1.
+ *         Otherwise - 0.
+ */
+int is_empty_line(char *line, char *delims)
+{
+	int i, j;
+
+	for (i = 0; line[i]; i++)
+	{
+		for (j = 0; delims[j]; j++)
+		{
+			if (line[i] == delims[j])
+				break;
+		}
+		if (delims[j] == '\0')
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * get_op_func - Matches an opcode with its corresponding function.
+ * @opcode: The opcode to match.
+ *
+ * Return: A pointer to the corresponding function.
+ */
+void (*get_op_func(char *opcode))(stack_t**, unsigned int)
+{
+	instruction_t op_funcs[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; op_funcs[i].opcode; i++)
+	{
+		if (strcmp(opcode, op_funcs[i].opcode) == 0)
+			return (op_funcs[i].f);
+	}
+
+	return (NULL);
+}
